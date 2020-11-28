cce4599390b54f63f9f2f285e94aa1e93cdeff8e
後藤卓磨 <gota.main@i.softbank.jp>
2020-11-28 22:26:48 +0900

    ログインバグ修正

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,13 +7,14 @@ class SessionController < ApplicationController
     err_msg << 'パスワードが入力されていません。' if params[:password].blank?
     render json: {err_msg: err_msg} and return if err_msg.present?
 
-      if params[:shop_login]
-        @login_data = Shop.find_by(login_id: params[:login_id])
-        position = 'shop'
-      else
-        @login_data = User.find_by(login_id: params[:login_id])
-        position = 'user'
-      end
+    if params[:shop_login]
+      @login_data = Shop.find_by(login_id: params[:login_id])
+      position = 'shop'
+    else
+      @login_data = User.find_by(login_id: params[:login_id])
+      position = 'user'
+    end
+
     if @login_data&.authenticate(params[:password])
       jwt_token = encode(@login_data.id)
       response.headers['X-Authentication-Token'] = jwt_token
